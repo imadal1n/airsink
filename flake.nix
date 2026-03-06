9f7a1ec6c9760d4fa0326ad9ce2ea013132cb5cc
limax <imadalin@gmail.com>
2026-03-06 10:26:34 +0200

    Add Nix flake with dev shell and build derivation
    rustPlatform.buildRustPackage for release builds, dev shell with mold linker, pipewire, avahi, openssl, dbus.

---
+++ flake.nix
@@ -0,0 +1,105 @@
+{
+  description = "airsink - AirPlay 2 audio streaming for Linux";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+      };
+    in
+    {
+      packages.${system}.default = pkgs.rustPlatform.buildRustPackage rec {
+        pname = "airsink";
+        version = "0.1.0";
+
+        src = builtins.path {
+          path = ./.;
+          name = "airsink-source";
+        };
+
+        cargoLock.lockFile = ./Cargo.lock;
+
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+          clang
+        ];
+
+        buildInputs = with pkgs; [
+          # pipewire (native audio capture + virtual sink)
+          pipewire
+
+          # avahi / mdns (device discovery)
+          avahi
+
+          # crypto libs
+          openssl
+
+          # dbus (avahi bindings, pipewire comms)
+          dbus
+        ];
+
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+        meta = with pkgs.lib; {
+          description = "AirPlay 2 audio streaming for Linux";
+          license = licenses.mit;
+          platforms = [ "x86_64-linux" ];
+        };
+      };
+
+      checks.${system} = {
+        default = self.packages.${system}.default;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        name = "airsink";
+
+        nativeBuildInputs = with pkgs; [
+          # rust toolchain
+          rustToolchain
+          pkg-config
+          clang
+          mold
+        ];
+
+        buildInputs = with pkgs; [
+          # pipewire (native audio capture + virtual sink)
+          pipewire
+
+          # avahi / mdns (device discovery)
+          avahi
+
+          # crypto libs
+          openssl
+
+          # dbus (avahi bindings, pipewire comms)
+          dbus
+        ];
+
+        LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+
+        # link faster with mold
+        CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER = "clang";
+        CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
+
+        shellHook = ''
+          echo "airsink dev shell"
+          echo "rust: $(rustc --version)"
+        '';
+      };
+    };
+}
